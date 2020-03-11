49be27a73686db06a3fb9b75e75483b6264cfc57
Sri Datta <b.sridatta@gmail.com>
2020-03-11 21:02:24 +0100

    gpu optimization and subsetsampler

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,50 +1,56 @@
-import logging as log
+import logging
 import torch
 from dataset import BengaliAI
-
+from torch.utils.data import SubsetRandomSampler
 
 def train_dataloader(params):
-    log.info('Training data loader called')
-
     dataset = BengaliAI(folds=params.train_folds, train=True, 
                         data_root=params.data_root)
+    
+    sampler = SubsetRandomSampler(range(2*params.batch_size)) if params.fast_dev_run else None
 
     loader = torch.utils.data.DataLoader(
         dataset=dataset,
         batch_size=params.batch_size,
         num_workers=params.num_workers,
-        pin_memory=params.pin_memory
+        pin_memory=params.pin_memory,
+        sampler=sampler
     )
+    logging.info(f'Training data loader called. len - {len(loader)*params.batch_size}')
 
     return loader
 
 def val_dataloader(params):
-    log.info('Validation data loader called')
-
     dataset = BengaliAI(folds=params.val_folds, train=True, 
                         data_root=params.data_root)
+
+    sampler = SubsetRandomSampler(range(params.batch_size)) if params.fast_dev_run else None
 
     loader = torch.utils.data.DataLoader(
         dataset=dataset,
         batch_size=params.batch_size,
         num_workers=params.num_workers,
-        pin_memory=params.pin_memory
+        pin_memory=params.pin_memory,
+        sampler=sampler
     )
+    logging.info(f'Validation data loader called. len - {len(loader)*params.batch_size}')
 
     return loader
 
 def test_dataloader(params):
-    log.info('Test data loader called')
-
     dataset = BengaliAI(train=False, test_id=params.test_id,
                         data_root=params.data_root)
+
+    sampler = SubsetRandomSampler(range(params.batch_size)) if params.fast_dev_run else None
 
     loader = torch.utils.data.DataLoader(
         dataset=dataset,
         batch_size=params.batch_size,
         num_workers=params.num_workers,
-        pin_memory=params.pin_memory
+        pin_memory=params.pin_memory,
+        sampler=sampler
     )
+    logging.info(f'Test data loader called. len - {len(loader)*params.batch_size}')
 
     return loader
 
