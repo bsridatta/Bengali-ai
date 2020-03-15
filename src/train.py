791416eeff1c7fe6f55504e90bad3949cc720914
Sri Datta <b.sridatta@gmail.com>
2020-03-15 17:20:57 +0100

    tboard logs, dp, freeze

--- src/train.py
+++ src/train.py
@@ -10,6 +10,7 @@ import torch
 import data_loader
 from models import EfficientNetB5
 from trainer import training_epoch, validation_epoch
+from torch.utils.tensorboard import SummaryWriter
 
 def main():
     # Experiment Configuration
@@ -17,7 +18,14 @@ def main():
     config = parser.parse_args()
     torch.manual_seed(config.seed)
     logging.getLogger().setLevel(logging.INFO)
-
+    
+    # Tensorboard Logs
+    suffix = 0
+    while os.path.exists(f"../logs/{config.exp_name}_{suffix}"):
+        suffix+=1
+    writer = SummaryWriter(f"../logs/{config.exp_name}_{suffix}")
+    config.writer = writer
+        
     # GPU setup    
     use_cuda = config.cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
@@ -26,6 +34,8 @@ def main():
     config.num_workers = 4 if use_cuda else 4 # for dataloader
     config.pin_memory = False if use_cuda else False
 
+    writer.add_text("config", str(config))    
+ 
     # Data loading
     train_loader = data_loader.train_dataloader(config)
     val_loader = data_loader.val_dataloader(config)
@@ -33,14 +43,28 @@ def main():
     
     # Model
     model = EfficientNetB5(config.pretrained)
+    
+    # Freeze blocks
+    if config.freeze_blocks:
+        set_req_grad = False 
+        for param in model.named_parameters():
+            if f'_blocks.{config.freeze_blocks}' not in param[0]:
+                param[1].requires_grad = set_req_grad
+            else:
+                break
+
+    # For multiple GPUs
+    if torch.cuda.device_count() > 1:
+        logging.info(f'Using {torch.cuda.device_count()} GPUs')
+        model = torch.nn.DataParallel(model)
     model.to(device)
 
-    # Optims - Scheduler monitors recall metric, hence mode = max
+    # Optims
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', 
                                                            patience=5,
                                                            factor=0.3, verbose=True)
-
+    # Resume training
     if config.resume_pt:
         logging.info(f'Loading {config.resume_pt}')
         state = torch.load(f'{config.save_dir}/{config.resume_pt}')
@@ -50,6 +74,8 @@ def main():
     logging.info('Start training procedure')
     val_loss_min = float('inf')
 
+    # Training
+    config.step = 0
     for epoch in range(1, config.epochs+1):
         training_epoch(config, model, train_loader, optimizer, epoch)
         val_loss = validation_epoch(config, model, val_loader)
@@ -57,13 +83,22 @@ def main():
         
         if val_loss < val_loss_min:
             val_loss_min = val_loss
+            try:
+                state_dict = model.module.state_dict()
+            except AttributeError:
+                state_dict = model.state_dict()
+
             state = {
                 'epoch': epoch,
                 'val_loss': val_loss,
-                'model_state_dict': model.state_dict(),
+                'model_state_dict': state_dict,
                 'optimizer_state_dict': optimizer.state_dict()
             }
-            torch.save(state, f'{config.save_dir}/{config.exp_name}_{val_loss}.pt')
+            torch.save(state, f'{config.save_dir}/{config.exp_name}.pt')
+            logging.info(f'Saved pt: {config.save_dir}/{config.exp_name}.pt')
+
+
+    writer.close()
 
 def training_specific_args():  
 
@@ -81,13 +116,16 @@ def training_specific_args():
         # network params
         parser.add_argument('--pretrained', default=False, type=lambda x: (str(x).lower() == 'true'))
         parser.add_argument('--learning_rate', default=1e-4, type=float)
+        parser.add_argument('--freeze_blocks', default=0, type=int)
         
         # training params
+        parser.add_argument('--cutmix', default=0, type=float)
+        
         parser.add_argument('--epochs', default=2, type=int)
         parser.add_argument('--batch_size', default=4, type=int)
         
         parser.add_argument('--fast_dev_run', default=True, type=lambda x: (str(x).lower() == 'true'))
-        parser.add_argument('--resume_pt', type=str)
+        parser.add_argument('--resume_pt', default = 0, type=str)
 
         # output
         parser.add_argument('--save_dir', default=f'{os.path.dirname(os.getcwd())}/checkpoints', type=str)
